e31aa49181231e988cc18f395380e4782e26815b
AALUND13 <tamlund1976@gmail.com>
2024-04-26 16:10:28 -0400

    Update typing

--- typings/utilis/JavaScriptExecutor.d.ts
+++ typings/utilis/JavaScriptExecutor.d.ts
@@ -3,7 +3,7 @@
  * @param {string | Function} code - The JavaScript code to execute.
  * @param {Object} args - Arguments to pass to the code.
  * @param {NodeRequire} [requireFn] - The require function to use when executing the code.
- * @param {number} [cutoff=5] - The maximum length of console output to capture.
+ * @param {number} [cutoff=Infinity] - The maximum length of console output to capture.
  * @returns {Promise<{returnValue: any, consoleOutput: string, error: string | undefined}>} - An object containing the return value, console output, and error message (if any).
  */
 export function executeJavaScript(code: string | Function, args?: any, requireFn?: NodeRequire, cutoff?: number): Promise<{
